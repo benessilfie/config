0058b44e5fd5c80fb6f7b3fac020da1cfa624ee5
Essilfie <bquansah007@gmail.com>
2024-01-06 21:05:57 +0000

    Delete old config for neovim

--- nvim/lua/ben/tmux-settings.lua
+++
@@ -1,9 +0,0 @@
---  Toggle tmux statusline
-vim.fn.system('tmux set-option status off')
-
-vim.cmd [[
-  augroup LunarVim
-    autocmd!
-    autocmd VimLeavePre * silent !tmux set-option status on
-  augroup END
-]]
