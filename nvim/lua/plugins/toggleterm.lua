126b5a91cc3e32d711f8267ac52b3ddb4f8b198d
Essilfie <bquansah007@gmail.com>
2024-01-07 21:19:24 +0000

    Fix toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,25 +3,25 @@ return {
   version = "*",
   config = function()
     require("toggleterm").setup({
-      size = 17,
-      open_mapping = [[<c-t>]],
-      shade_filetypes = {},
+      size = 16,
+      direction = "horizontal",
+      open_mapping = [[<c-\>]],
+      autochdir = true,
       shade_terminals = true,
       shading_factor = 2,
       start_in_insert = true,
-      persist_size = false,
-      direction = "horizontal",
-      autochdir = true,
-      close_on_exit = true,
-      shell = vim.o.shell,
-      float_opts = {
-        border = "curved",
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
-      },
+      -- shade_filetypes = {},
+      -- persist_size = false,
+      -- close_on_exit = true,
+      -- shell = vim.o.shell,
+      -- float_opts = {
+      --   border = "curved",
+      --   winblend = 0,
+      --   highlights = {
+      --     border = "Normal",
+      --     background = "Normal",
+      --   },
+      -- },
     })
   end,
 }
